c36d2fc89a4d292f23a7c28f2b9b21ee67a20bef
groc.incognito <groc.incognito@gmail.com>
2016-05-04 03:42:08 -0400

    debut chifoumi.php

---
+++ chifoumi.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Chifoumi</title>
+</head>
+
+<body>
+  <?php
+  ?>
+    <form method="get" action="chifoumi.php">
+      <legend>
+        Pierre,Feuille,Ciseaux
+      </legend>
+      <p>
+        Coup ?
+        <a href="?coup=0"><img href="./images/pierre.png"></a>
+        <a href="?coup=1"><img href="./images/feuille.png"></a>
+        <a href="?coup=2"><img href="./images/ciseaux.png"></a>
+      </p>
+    </form>
+</body>
+
+</html>
